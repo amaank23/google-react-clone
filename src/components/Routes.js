d390b569dc1a6c5836d2bb059efa623ea6f00f45
aman-khan-laptop <amaanwaseem24@hotmail.com>
2022-07-09 04:22:54 +0500

    Added Basic Design Components and Routes Configured

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Routes as ReactRoutes, Route, Navigate } from 'react-router-dom'
+import Results from './Results'
 
 const Routes = () => {
   return (
-    <div>Routes</div>
+    <div className='p-4'>
+        <ReactRoutes>
+            <Route path="/" element={<Navigate to="/search"/>} />
+            {['/search', '/images', '/news', '/videos'].map((item, index) => (
+                <Route key={index} path={item} element={<Results />} />
+            ))}
+        </ReactRoutes>
+    </div>
   )
 }
 
